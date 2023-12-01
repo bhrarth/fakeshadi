b7a2e5ac2603ed8f38331323d3c7438234d02aec
Bharath BM <91864244+bhrarth@users.noreply.github.com>
2023-12-02 00:59:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,13 +10,7 @@ app.use(bodyParser.json());
 const allowedOrigins = ['http://localhost:3000','https://bhrarth.github.io/fakeshadi/']
 // Use CORS middleware
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
+  origin:'*'
 }));
 
 // Route to handle form submissions
